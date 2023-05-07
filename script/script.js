71e2f662e0aa2adc517160b47eb23fd7105cfe3f
Trung Hiếu <trunghieuthegod@gmail.com>
2023-05-07 10:47:41 +0700

    07/05/2023_TranTrungHieu_MainPage_Body_NavBar_Finish

---
+++ script/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    //navbar-acction
+    $(".navbar-item").hover(function (e) {
+        $(this).children(".nav-item-listText").css("display", "flex")
+
+    }, function () {
+        $(this).children(".nav-item-listText").css("display", "none")
+    }
+    );
+    //textBody-action
+    let myLink = document.querySelector(".textBody");
+    myLink.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "404_notification.html ";
+    });
+    //owl-acction
+    $("#owl-example").owlCarousel({
+        items: 1,
+        autoPlay: true,
+        navigation: true,
+        navigationText: [`<i class="fa-solid fa-angle-left"></i>`, `<i class="fa-solid fa-angle-right"></i>`],
+    });
+    $("#owl-example1").owlCarousel({
+        items: 5,
+
+        autoPlay: true,
+        pagination: false,
+
+    });
+});
+//Xóa phần js của btn có sẵn trong BS5
